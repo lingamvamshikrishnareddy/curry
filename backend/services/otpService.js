11725401ce502606d98f10022eab73e55a1f37f1
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2024-11-07 15:16:59 +0530

    Delete backend/services directory

--- backend/services/otpService.js
+++
@@ -1,7 +0,0 @@
-exports.generateOTP = () => {
-    return Math.floor(100000 + Math.random() * 999999).toString();
-  };
-  
-  exports.verifyOTP = (inputOTP, storedOTP) => {
-    return inputOTP === storedOTP;
-  };
