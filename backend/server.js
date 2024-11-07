901c140740b38f90433f4d6f9531aed5aee0b336
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2024-11-07 21:10:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -95,7 +95,9 @@ app.use('*', (req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 module.exports = server;
